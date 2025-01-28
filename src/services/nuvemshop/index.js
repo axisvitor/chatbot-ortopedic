3b2151271afbf20226bf109a5ec9e5391587abfd
axisvitor <axisvanguard.agencia@gmail.com>
2025-01-28 17:33:57 -0300

    Atualização completa do chatbot com novas funcionalidades

--- src/services/nuvemshop/index.js
+++ src/services/nuvemshop/index.js
@@ -1,4 +1,4 @@
-const logger = require('../../../utils/logger');
+const logger = require('../../utils/logger');
 const moment = require('moment-timezone');
 const { NuvemshopBase } = require('./base');
 const { OrderService } = require('./services/order');
